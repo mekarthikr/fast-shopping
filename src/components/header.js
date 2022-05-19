f4654634679a6c23a172685df751bb4cf7636377
Karthik R <iamjustkarthik@gmail.com>
2022-05-19 23:43:21 +0530

    Structured Folders

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <nav className='nav' >
+                    <ul><a>hole</a></ul>
+                    <ul><a>hole</a></ul>
+                    <ul><a>hole</a></ul>
+                    
+                </nav>
+            </div>
+        );
+    }
+}
